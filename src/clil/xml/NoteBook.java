3e381da097f849f0a916bf76d0c4d63ab70d8830
SSLAVIERO <SSLAVIERO@labsis12.itis.lan>
2018-11-22 10:29:55 +0100

    versione 1.0

---
+++ src/clil/xml/NoteBook.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clil.xml;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author SSLAVIERO
+ */
+public class NoteBook {
+    ArrayList<Note> notes=new ArrayList();
+    ArrayList<Note> notes_word=new ArrayList();
+    ArrayList<Note> notes_date=new ArrayList();
+    Date data =new Date();
+    
+    public void AddNote(String description)
+    {
+        notes.add(new Note(description+data.getDay()+(data.getMonth()+1)+(data.getYear()+1900)));
+    }
+    
+    public void AddNote(Note t)
+    {
+        notes.add(t);
+    }
+    
+    public ArrayList<Note> GetNoteByDescription(String word)
+    {
+        notes_word.removeAll(notes);
+        
+        for (int i = 0; i < notes.size(); i++) {
+             if(notes.get(i).getDescription().compareTo(word)==0)
+             {
+                 notes_word.add(notes.get(i));
+             }           
+             
+        }
+        return notes_word;
+    }
+    public ArrayList<Note> GetNotesByDate(Date date)
+    {
+        notes_date.removeAll(notes);
+        
+        for (int i = 0; i < notes.size(); i++) {
+             if(notes.get(i).getData().compareTo(date)==0)
+             {
+                 notes_date.add(notes.get(i));
+             }           
+             
+        }
+        return notes_word;
+    }
+    
+}
