d118ea483f05753ba27bcb3ee49f5962384fc2cf
SSLAVIERO <SSLAVIERO@labsis12.itis.lan>
2018-11-22 10:30:11 +0100

    versione 1.0

---
+++ src/clil/xml/Note.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clil.xml;
+
+import java.util.Date;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author SSLAVIERO
+ */
+
+@XmlRootElement
+public class Note {
+
+    Date data;
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+    @XmlElement
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Note(String description) {
+        this.description=description;
+        data=new Date();
+    }
+    String description;
+    
+    
+    
+}
